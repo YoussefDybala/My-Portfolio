7abaca3774730a4fe6b471efff0c1ee0c78f5457
Youssef Dybala <newyossef55o@gmail.com>
2024-09-05 08:55:07 +0300

    new commit

--- JavaScript/script.js
+++
@@ -1,9 +0,0 @@
-let toggler = document.querySelector(".tog")
-let links = document.querySelector(".links")
-let exit = document.querySelector(".x")
-toggler.addEventListener("click" , function () {
-    links.classList.add ("down")
-})
-exit.addEventListener("click" , function () {
-    links.classList.remove ("down")
-})
